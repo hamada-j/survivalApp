d16c5ed0b6b57b140a7123d12594a32abf9c1729
Hamada <defjamvk@gmail.com>
2021-05-25 21:04:22 +0200

    Create feature and add Got fetch AJAX. Fix #3
    `https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/${mag}_${frec}.geojson`;

--- equakes-module.js
+++ equakes-module.js
@@ -1,3 +1,42 @@
 
-const url = `https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/${mag}_${frec}.geojson`;
+const got = require('got');
+
+(async () => {
+	try {
+		const response = await got('https://sindresorhus.com');
+		console.log(response.body);
+		//=> '<!doctype html> ...'
+	} catch (error) {
+		console.log(error.response.body);
+		//=> 'Internal server error ...'
+	}
+})();
+
+
+const timesChoice = ["month", "day", "week", "hour"]
+const magsChoice = ["all", "4.5", "2.5", "1.0", "significant"]
+
+// month day week hour
+// all, 4.5, 2.5, 1.0 significant
+
+function getEquakes( mag = "significant", time= "month"){
+    if (!magsChoice.includes(mag) || !timesChoice.includes(time)){
+        return "Bad chose Bro!"
+    }
+
+    const url = `https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/${mag}_${time}.geojson`;
+
+    got(url).then(res => console.log(JSON.parse(res.body).features)).catch(err => console.log(err))
+
+    return url;
+}
+
+//getEquakes("all", "month")
+//console.log(getEquakes("all", "month"));
+
+
+
+
+
+
 
