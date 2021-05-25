8b9c832cbd410744f85892b0e22e8fa48a23aec5
Hamada <defjamvk@gmail.com>
2021-05-25 21:19:14 +0200

    Refactor
    Bug return HTML from the API

--- equakes-module.js
+++ equakes-module.js
@@ -1,4 +1,4 @@
-
+/*
 const got = require('got');
 
 (async () => {
@@ -11,7 +11,6 @@ const got = require('got');
 		//=> 'Internal server error ...'
 	}
 })();
-
 
 const timesChoice = ["month", "day", "week", "hour"]
 const magsChoice = ["all", "4.5", "2.5", "1.0", "significant"]
@@ -26,17 +25,49 @@ function getEquakes( mag = "significant", time= "month"){
 
     const url = `https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/${mag}_${time}.geojson`;
 
-    got(url).then(res => console.log(JSON.parse(res.body).features)).catch(err => console.log(err))
+    got(url).then(res => console.log(JSON.parse(res.body).features)).catch(err => err.response.body)
 
     return url;
 }
 
 //getEquakes("all", "month")
 //console.log(getEquakes("all", "month"));
+*/
+
+/*
+FOR TEST
+
+function getQuakes (mag='significant', time="hour")  {
+    return new Promise((resolve,reject) => {
+        
+        if(!magsChoices.includes(mag) || !timesChoices.includes(time)){
+            reject("bad choice my friend!");
+        }
+        
+        got(`https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/${mag}_${time}.geojson`)
+            .then(res => resolve(JSON.parse(res.body).features))
+            .catch(error => reject(error.response.body));
+    });
+};
+
+getQuakes("all", "month").then(res => console.log(res))
+*/
 
 
+const got = require('got');
 
+const timesChoices = ["hour", "day", "week", "month"];
+const magsChoices = ['all', '1.0', '2.5', '4.5', 'significant'];
 
-
-
-
+exports.getEarthquakes = (mag='significant', time="hour") => {
+    return new Promise((resolve,reject) => {
+        
+        if(!magsChoices.includes(mag) || !timesChoices.includes(time)){
+            reject("bad choice my friend!");
+        }
+        
+        got(`https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/${mag}_${time}.geojson`)
+            .then(res => resolve(JSON.parse(res.body).features))
+            .catch(error => reject(error.response.body));
+    });
+};
