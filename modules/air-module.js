7706470411fbc2bed4770134c4d1c374d1414c79
Hamada <defjamvk@gmail.com>
2021-05-26 20:29:51 +0200

    Structur module

--- modules/air-module.js
+++ modules/air-module.js
@@ -1,13 +1,14 @@
 const got = require('got');
 const {tokens, defaults} = require('../config');
+const getData = require('../util/getData');
 
-function getData (url) {
-    return new Promise((resolve, reject) => {
-        got(url)
-            .then(res => resolve(JSON.parse(res.body)))
-            .catch(error => reject(error.response.body));
-    });
-}
+// function getData (url) {
+//     return new Promise((resolve, reject) => {
+//         got(url)
+//             .then(res => resolve(JSON.parse(res.body)))
+//             .catch(error => reject(error.response.body));
+//     });
+// }
 
 exports.uvNow = (lat=defaults.lat, lon=defaults.lon) => getData(`https://api.openweathermap.org/data/2.5/uvi?lang=es&lat=${lat}&lon=${lon}&appid=${tokens.owm}`);
 exports.uvForecast = (lat=defaults.lat, lon=defaults.lon) => getData(`https://api.openweathermap.org/data/2.5/uvi/forecast?lang=es&lat=${lat}&lon=${lon}&appid=${tokens.owm}`);
